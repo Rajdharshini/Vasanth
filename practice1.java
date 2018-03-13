9af3c775d1358550767552df41e5588c49fd8d55
Rajdharshini <rajvasanth307@gmail.com>
2018-03-13 15:38:31 +0530

    Create practice1.java

---
+++ practice1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		{
+			char c=s.next().charAt(0);
+			if(Character.isLetter(c))
+			{
+				System.out.print("Alphabed");
+			}
+			else
+			{
+				System.out.print("No");
+			}
+		}
+	}
+}
